cb5dd6f27fdd75d811218f50ad88345d9e66c1ae
JK_RUSH <289280282@qq.com>
2019-03-22 15:06:20 +0800

    调试debug

--- base/HTMLTestReportCN.py
+++ base/HTMLTestReportCN.py
@@ -185,9 +185,9 @@ class Template_mixin(object):
     2: '错误',
     }
 
-    DEFAULT_TITLE = '单元测试报告'
+    DEFAULT_TITLE = '接口自动化测试报告'
     DEFAULT_DESCRIPTION = ''
-    DEFAULT_TESTER='最棒QA'
+    DEFAULT_TESTER='ROOT'
 
     # ------------------------------------------------------------------------
     # HTML Template
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import unittest
 from tests import APITest
 from base import HTMLTestReportCN
+import HTMLTestRunner
 import time
 
 suite = unittest.TestSuite()
@@ -11,4 +12,4 @@ suite.addTest(APITest('test_02'))
 now = time.strftime('%Y%m%d_%H%M%S')
 with open('./report/%s.html' % now, 'wb') as f:
     # with open('./report/index.html', 'wb') as f:
-    HTMLTestReportCN.HTMLTestRunner(stream=f, title='接口自动化报告').run(suite)
+    HTMLTestRunner.HTMLTestRunner(stream=f, title='接口自动化报告', verbosity=2, retry=1).run(suite)
