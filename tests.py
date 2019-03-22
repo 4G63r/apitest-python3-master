8f493128662650ae773cab9bcb82c07070c83cd3
JK_RUSH <289280282@qq.com>
2019-03-22 14:08:23 +0800

    调试debug

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import requests
+
+
+class APITest(unittest.TestCase):
+    def test_01(self):
+        a = 1
+        b = 2
+        self.assertTrue(a == b)
+
+    def test_02(self):
+        print(2222)
